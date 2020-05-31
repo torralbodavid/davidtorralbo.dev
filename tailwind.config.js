67e6fa47d641b448d122b39e65f61d2740e35277
David Torralbo <davidtorralboperez@gmail.com>
2020-05-31 17:42:12 +0200

    Change inter var to Helvetica Neue

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                sans: ['Helvetica Neue', ...defaultTheme.fontFamily.sans],
             },
         },
     },
