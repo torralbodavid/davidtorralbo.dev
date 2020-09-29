4fc2698724d741156333db96dde7aa281487c006
David Torralbo <davidtorralboperez@gmail.com>
2020-09-29 19:46:39 +0200

    Delete wrong imports

--- app/Http/Livewire/Page.php
+++ app/Http/Livewire/Page.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Livewire;
 
-use GNAHotelSolutions\ImageCacher\Cacher;
-use GNAHotelSolutions\ImageCacher\Image;
 use Livewire\Component;
 use Wink\WinkPage;
 
@@ -13,10 +11,6 @@ class Page extends Component
 
     public function mount($slug)
     {
-        $image = new Image('david-torralbo-hero-section.jpg', 'img');
-
-        $resized = (new Cacher())->resize($image, 1920, 1080); // Get a smaller version of the image or the same if the size is smaller.
-        dd($resized);
         $page = WinkPage::where('slug', $slug)->first();
 
         if($page === null) {
