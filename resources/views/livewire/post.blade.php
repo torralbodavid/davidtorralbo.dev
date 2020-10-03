382ed32d616017e7d350574970f7e4a06c83ef11
David Torralbo <davidtorralboperez@gmail.com>
2020-10-03 12:13:42 +0200

    Update post header

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -1,37 +1,48 @@
 <article class="relative py-16 bg-white overflow-hidden">
     <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
         <div class="relative h-full text-lg max-w-prose mx-auto">
-            <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+            <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none"
+                 viewBox="0 0 404 384">
                 <defs>
-                    <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                    <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20"
+                             patternUnits="userSpaceOnUse">
+                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor"/>
                     </pattern>
                 </defs>
-                <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+                <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"/>
             </svg>
-            <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+            <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384"
+                 fill="none" viewBox="0 0 404 384">
                 <defs>
-                    <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                    <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20"
+                             patternUnits="userSpaceOnUse">
+                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor"/>
                     </pattern>
                 </defs>
-                <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+                <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"/>
             </svg>
-            <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+            <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none"
+                 viewBox="0 0 404 384">
                 <defs>
-                    <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                    <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20"
+                             patternUnits="userSpaceOnUse">
+                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor"/>
                     </pattern>
                 </defs>
-                <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+                <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"/>
             </svg>
         </div>
     </div>
     <div class="relative px-4 sm:px-6 lg:px-8">
-        <div class="text-lg max-w-prose mx-auto mb-6">
-            <p class="text-base text-center leading-6 text-teal-600 font-semibold tracking-wide uppercase">Posts</p>
-            <h2 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">{{ $post->title }}</h2>
-            <p class="text-xl text-gray-500 leading-8">{{ $post->excerpt }}</p>
+        <div class="max-w-screen-xl mx-auto">
+            <div class="text-center mb-14">
+                @php
+                    $created = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $post->created_at);
+                @endphp
+                <span class="text-base leading-6 font-semibold text-indigo-600 tracking-wide uppercase" title="{{ $created->format('d-m-Y H:i') }}">{{ $created->diffForHumans() }}</span>
+                <h1 class="mt-1 text-4xl leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">{{ $post->title }}</h1>
+                <p class="max-w-xl mt-5 mx-auto text-xl leading-7 text-gray-500">{{ $post->excerpt }}</p>
+            </div>
         </div>
         <div class="prose prose-lg text-gray-500 mx-auto">
             {!! $post->content !!}
