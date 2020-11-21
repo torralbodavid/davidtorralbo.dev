9e0160d2d462e2f39ac2748b9cfdcfe5bcabfd5a
David Torralbo <davidtorralboperez@gmail.com>
2020-11-22 00:37:45 +0100

    Update menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -18,7 +18,7 @@
                     Entradas
                 </a>
 
-                <a href="#" class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
+                <a href="{{ route('page', 'acerca-de') }}" class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
                     Sobre mi
                 </a>
 
@@ -26,7 +26,7 @@
                     Mis proyectos
                 </a>
 
-                <a href="#" class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
+                <a href="{{ route('contact') }}" class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
                     Contacto
                 </a>
             </nav>
