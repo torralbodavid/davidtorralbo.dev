deb7491d2e323a1fbf931303e7448fe907a92471
David Torralbo <davidtorralboperez@gmail.com>
2020-11-29 02:08:56 +0100

    Fix page margins

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,6 +1,6 @@
 @section('title', 'The programmer\'s blog')
 
-<div class="relative pt-16 px-4 sm:px-6 lg:pt-24 lg:px-8">
+<div class="relative px-4 sm:px-6 lg:px-8">
     <div class="inset-0">
         <div class="bg-white h-1/3 sm:h-2/3"></div>
     </div>
