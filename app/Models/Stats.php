350146f7810039be4c54cc07fe0f06da6867331e
David Torralbo <davidtorralboperez@gmail.com>
2020-12-04 12:27:18 +0100

    Add new stats migration and model

---
+++ app/Models/Stats.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stats extends Model
+{
+    use HasFactory;
+
+    protected $table = 'github_stats';
+    protected $fillable = ['username', 'repositories', 'stargazers', 'contributions'];
+}
