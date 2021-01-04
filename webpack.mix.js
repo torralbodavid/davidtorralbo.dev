d947a0f595271baf083093afe3a21f00c23023b9
David Torralbo <davidtorralboperez@gmail.com>
2021-01-04 21:53:29 +0100

    Fix assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,7 @@ require("laravel-mix-tailwind");
  |
  */
 
-mix.js("resources/js/app.js", "public/js/app.js")
-    .sass("resources/sass/app.scss", "public/css/app.css")
+mix.sass("resources/sass/app.scss", "public/css/app.css")
     .tailwind("./tailwind.config.js")
     .sourceMaps();
 
