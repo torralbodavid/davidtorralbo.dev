1f120cc634360036d6494a8debc40b7522a7cb10
David Torralbo <davidtorralboperez@gmail.com>
2021-01-04 23:04:46 +0100

    Add analytics at the bottom

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -21,25 +21,6 @@
         <meta name="msapplication-TileImage" content="{{ asset('favicons/ms-icon-144x144.png') }}">
         <meta name="theme-color" content="#ffffff">
 
-        <script type="text/javascript">
-            document.addEventListener("DOMContentLoaded", function(e) {
-                var script = document.createElement('script');
-                script.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-L23C2SCP7M');
-                script.setAttribute('type', 'text/javascript');
-                document.getElementsByTagName('head')[0].appendChild(script);
-
-                window.dataLayer = window.dataLayer || [];
-
-                function gtag() {
-                    dataLayer.push(arguments);
-                }
-
-                gtag('js', new Date());
-
-                gtag('config', 'G-L23C2SCP7M', {cookie_flags: 'SameSite=None;Secure'});
-            });
-        </script>
-
         @hasSection('title')
             <title>@yield('title') - {{ config('app.name') }}</title>
         @else
@@ -69,5 +50,23 @@
             @livewireScripts
         @endif
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" media="print" onload="this.media='all'">
+        <script type="text/javascript">
+            document.addEventListener("DOMContentLoaded", function(e) {
+                var script = document.createElement('script');
+                script.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-L23C2SCP7M');
+                script.setAttribute('type', 'text/javascript');
+                document.getElementsByTagName('head')[0].appendChild(script);
+
+                window.dataLayer = window.dataLayer || [];
+
+                function gtag() {
+                    dataLayer.push(arguments);
+                }
+
+                gtag('js', new Date());
+
+                gtag('config', 'G-L23C2SCP7M', {cookie_flags: 'SameSite=None;Secure'});
+            });
+        </script>
     </body>
 </html>
