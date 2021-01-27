f4b514555f31481afb330d113517eecdc89fee9f
David Torralbo <davidtorralboperez@gmail.com>
2021-01-27 19:26:44 +0100

    Update contribution text

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -81,7 +81,7 @@
                         </div>
                         <div class="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
                             <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                                contributions (in the last year)
+                                contributions
                             </dt>
                             <dd class="order-1 text-5xl leading-none font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-600 via-purple-500 to-blue-700">
                                 {{ $stats->contributions }}
