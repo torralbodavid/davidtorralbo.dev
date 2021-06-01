2612d9dc746891daf90602ce2900cc2ed96aa14f
David Torralbo <davidtorralboperez@gmail.com>
2021-06-01 19:03:26 +0200

    Add rememberable

--- app/Models/Stats.php
+++ app/Models/Stats.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Watson\Rememberable\Rememberable;
 
 class Stats extends Model
 {
+    use Rememberable;
     use HasFactory;
 
+    protected $rememberFor = 60 * 120;
     protected $table = 'github_stats';
     protected $fillable = ['username', 'repositories', 'stargazers', 'contributions'];
 }
