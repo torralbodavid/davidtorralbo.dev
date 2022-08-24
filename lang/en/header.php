1eb933f2be1f760fe64b81a536b8286f877f0ad1
David Torralbo <davidtorralboperez@gmail.com>
2022-08-24 18:00:30 +0200

    Add english header translations

---
+++ lang/en/header.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'blog' => 'Blog',
+    'talks' => 'Talks',
+    'about_me' => 'About me',
+    'uses' => 'Uses',
+    'contact' => 'Contact',
+    'seo_audit' => 'Website seo audit',
+
+    'featured' => [
+        'first' => [
+            'title' => 'Learn more',
+            'description' => 'Check out my last blog posts'
+        ],
+        'second' => [
+            'title' => 'Check your website\'s health',
+            'description' => 'Audit your website for free'
+        ],
+        'third' => [
+            'title' => 'May I help you?',
+            'description' => 'Get in touch with me!'
+        ]
+    ]
+];
